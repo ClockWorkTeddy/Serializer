8d866901f3788b926ee74b1d16949aae77575b47
ivan zhukov <vankaent@live.com>
2023-05-22 20:02:27 +0300

    Init, add some string builder and stream logic

--- Serializer/YourImplementation.cs
+++ Serializer/YourImplementation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using SerializerTests.Interfaces;
 using SerializerTests.Nodes;
@@ -27,7 +28,30 @@ namespace SerializerTests.Implementations
 
         public Task Serialize(ListNode head, Stream s)
         {
-            throw new NotImplementedException();
+            StringBuilder result = new("{");
+
+            while (head != null)
+            {
+                result.Append(WriteNode(head));
+                head = head.Next;
+            }
+
+            s.Write(Encoding.Default.GetBytes(result.ToString()));
+
+            return Task.CompletedTask;
+        }
+
+        private StringBuilder WriteNode(ListNode node)
+        {
+            StringBuilder result = new();
+
+            result.Append(node.ToString() + ": {");
+            result.Append($"Data: {node.Data},");
+            result.Append($"Previous: {node.Previous.ToString()}");
+            result.Append($"Random: {node.Random.ToString()}");
+            result.Append($"Next: {node.Next.ToString()}");
+
+            return result;
         }
     }
 }
