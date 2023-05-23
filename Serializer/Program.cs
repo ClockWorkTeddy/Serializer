0a68244dbaf158880f1ad7840c2fc773385f4e07
ivan zhukov <vankaent@live.com>
2023-05-23 22:43:33 +0300

    Changed Serialize method, some tests

--- Serializer/Program.cs
+++ Serializer/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SerializerTests.Implementations;
+using SerializerTests.Nodes;
+
+ListNode nodeB = new ListNode() { Data = "B" };
+ListNode nodeA = new ListNode() { Data = "A", Next = nodeB };
+nodeB.Previous = nodeA;
+
+JohnSmithSerializer serializer = new JohnSmithSerializer();
+using (FileStream fStream = new(@"D:\sample.json", FileMode.OpenOrCreate))
+{
+    serializer.Serialize(nodeA, fStream);
+    fStream.Close();
+}
--- Serializer/YourImplementation.cs
+++ Serializer/YourImplementation.cs
@@ -28,30 +28,54 @@ namespace SerializerTests.Implementations
 
         public Task Serialize(ListNode head, Stream s)
         {
-            StringBuilder result = new("{");
+            StringBuilder jsonBody = new();
 
             while (head != null)
             {
-                result.Append(WriteNode(head));
+                if (head.Previous != null)
+                    jsonBody.Append(",\r\n");
+
+                jsonBody.Append(WriteNode(head));
+                
+
                 head = head.Next;
             }
 
-            s.Write(Encoding.Default.GetBytes(result.ToString()));
+            jsonBody = AddBrackets(jsonBody);
+
+            s.Write(Encoding.Default.GetBytes(jsonBody.ToString()));
 
             return Task.CompletedTask;
+        }
+
+        private StringBuilder AddBrackets(StringBuilder jsonBody)
+        {
+            StringBuilder result = new StringBuilder("{\r\n");
+            result.Append(jsonBody);
+            result.Append("\r\n}");
+
+            return result;
         }
 
         private StringBuilder WriteNode(ListNode node)
         {
             StringBuilder result = new();
 
-            result.Append(node.ToString() + ": {");
-            result.Append($"Data: {node.Data},");
-            result.Append($"Previous: {node.Previous.ToString()}");
-            result.Append($"Random: {node.Random.ToString()}");
-            result.Append($"Next: {node.Next.ToString()}");
+            string strResult = $$"""
+            "{{GetHashOrHull(node)}}": {
+                "Data": "{{node.Data}}",
+                "Previous": "{{GetHashOrHull(node.Previous)}}",
+                "Random": "{{GetHashOrHull(node.Random)}}",
+                "Next": "{{GetHashOrHull(node.Next)}}"
+            }
+            """;
+
+            result.Append(strResult);
 
             return result;
         }
+
+        private string GetHashOrHull(ListNode node) =>
+            node == null ? "null" : node.GetHashCode().ToString();
     }
 }
